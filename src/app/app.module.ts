4a1f5e0db349333010d2dd12f3f714e53b82a526
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-06-29 22:58:27 +0200

    Added the http client module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
   ],
   imports: [
     BrowserModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
