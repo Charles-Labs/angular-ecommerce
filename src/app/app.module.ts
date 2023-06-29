a583954fa52246e91a845f4691548911bd4f5ba8
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-06-29 22:59:59 +0200

    Added ProductService as a provider.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
@@ -11,10 +12,10 @@ import { HttpClientModule } from '@angular/common/http'
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
