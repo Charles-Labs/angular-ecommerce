57df9ceebb2b8267e7157d5c9b6550ec73d52f20
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-01 23:35:10 +0200

    Injected the product service to the list component.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
+  constructor(private productService: ProductService){}
 
 }
