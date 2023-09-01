250e8039cb6f6bcffee551a380045039117c3362
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-09-01 23:37:35 +0200

    Added the oninit implementation.

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -7,8 +8,16 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
-  constructor(private productService: ProductService,
-              private route: ActivatedRoute) {}
+export class ProductDetailsComponent implements OnInit {
+	product: Product;
+
+	constructor(private productService: ProductService,
+				private route: ActivatedRoute) {}
+
+	ngOnInit(): void{
+		this.route.paramMap.subscribe(() => {
+			this.handleProductDetails();
+		})
+	}
 
 }
