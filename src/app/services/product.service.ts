82a0956fa2cf6fb1a0487624811357383336a607
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-09-25 17:47:18 +0200

    feat: Add pagination support for product search

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -36,6 +36,16 @@ export class ProductService {
 		return this.getProducts(searchUrl);
 	}
 
+	searchProductsPaginate(thePage: number,
+							thePageSize: number,
+							theKeyword: string): Observable<GetResponseProducts> {
+		
+		const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+							+ `&page=${thePage}&size=${thePageSize}`;
+
+		return this.httpClient.get<GetResponseProducts>(searchUrl);
+	}
+
 	searchProducts(theKeyword: string): Observable<Product[]> {
 		const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
