c7af6279a8d9dec6957ccbd2326b2075f09bbecf
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-10-09 18:16:12 +0200

    feat: Add total price and total quantity observers

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,9 @@ import { CartItem } from '../common/cart-item';
 export class CartService {
 
   cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
 
   constructor() { }
 }
