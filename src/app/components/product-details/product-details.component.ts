9ad3ec5a9c877f9c5e862baf87a3c84cf53c30c7
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-10-16 17:46:18 +0200

    fix: Remove unused parameter from addToCart call and injected missing CartItem dependency

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
 import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
@@ -34,7 +35,9 @@ export class ProductDetailsComponent implements OnInit {
 	}
 
 	addToCart() {
-
+		console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+		const theCartItem = new CartItem(this.product);
+		this.cartService.addToCart(theCartItem);
 	}
 
 }
