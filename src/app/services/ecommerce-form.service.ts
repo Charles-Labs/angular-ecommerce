6b6b17d9d21994885d638e9e6850af0b454ca91f
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-11-16 23:49:28 +0200

    feat: Add method for getting credit card years

--- src/app/services/ecommerce-form.service.ts
+++ src/app/services/ecommerce-form.service.ts
@@ -19,4 +19,19 @@ export class EcommerceFormService {
 
     return of(data);
   }
+
+  getCreditCardYears(): Observable<number[]> {
+    let data: number[] = [];
+
+    // build an array for "Year" dropdown list
+    // - start at current year and loop for next 10 years
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+      data.push(theYear);
+    }
+
+    return of(data);
+  }
 }
