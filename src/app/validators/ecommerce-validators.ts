3a3e222362d1d2c5ecd536c4288bb89e39287c85
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-02-03 23:19:00 +0200

    feat: Add whitespace validation method

--- src/app/validators/ecommerce-validators.ts
+++ src/app/validators/ecommerce-validators.ts
@@ -1,2 +1,9 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
 export class EcommerceValidators {
+
+    // whitespace validation
+    static notOnlyWhitespace(control: FormControl): ValidationErrors{
+        
+    }
 }
