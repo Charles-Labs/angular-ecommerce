ad40d10263f83b9dba097985a65e81d68bda8da3
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-03-01 16:00:00 +0200

    fix: Fix null return error

--- src/app/validators/ecommerce-validators.ts
+++ src/app/validators/ecommerce-validators.ts
@@ -3,7 +3,7 @@ import { FormControl, ValidationErrors } from "@angular/forms";
 export class EcommerceValidators {
 
     // whitespace validation
-    static notOnlyWhitespace(control: FormControl): ValidationErrors{
+    static notOnlyWhitespace(control: FormControl): ValidationErrors | null{
 
         // check if string only contains whitespace
         if((control.value != null) && (control.value.trim().length === 0)){
